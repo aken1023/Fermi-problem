fb9d3859f882e4353964a583587da7afc5309d3b
aken1023 <aken1023@gmail.com>
2025-04-10 16:27:04 +0800

    設定 GitHub 倉庫...

--- push_to_github.bat
+++ push_to_github.bat
@@ -2,8 +2,14 @@
 chcp 65001
 cls
 
+echo 檢查 Git 倉庫狀態...
+if not exist .git (
+    echo 初始化 Git 倉庫...
+    git init
+)
+
 echo 設定 GitHub 倉庫...
-git remote set-url origin https://github.com/aken1023/fermi-estimation.git
+git remote add origin https://github.com/aken1023/Fermi-problem.git
 
 echo 正在更新本地倉庫...
 git add .
@@ -18,7 +24,7 @@ git commit -m "%commit_msg%"
 
 echo.
 echo 正在推送到 GitHub...
-git push
+git push -u origin master
 
 echo.
 echo 完成！
